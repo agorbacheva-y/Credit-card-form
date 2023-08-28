33fe61a68d1cfad0542fdddb1eaaa6eee687113c
agorbacheva-y <agorbacheva.y@gmail.com>
2023-08-28 13:27:20 +0200

    form input card nr validation

--- src/components/CardForm.js
+++ src/components/CardForm.js
@@ -1,4 +1,7 @@
 function CardForm() {
+
+
+
   return (
     <form className="form__cntr">
       <div className="form__name">
@@ -8,19 +11,19 @@ function CardForm() {
 
       <div className="form__number">
         <label>Card Number</label>
-        <input type="number" name="number" placeholder="e.g. 1234 5678 9123 0000" maxlength="16" required />
+        <input type="text" name="number" placeholder="e.g. 1234 5678 9123 0000" pattern="[0 - 9]" required />
       </div>
 
       <div className="form__exp">
         <label>Exp. Date (MM/YY)</label>
-        <input className="form__exp--month" type="number" name="expmonth" placeholder="MM" required />
-        <input className="form__exp--year" type="number" name="expyear" placeholder="YY" required />
+        <input className="form__exp--month" type="text" name="expmonth" placeholder="MM" pattern="[01 - 12]" required />
+        <input className="form__exp--year" type="text" name="expyear" placeholder="YY" maxlength="2" required />
         
       </div>
 
       <div className="form__cvc">
         <label>CVC</label>
-        <input type="number" name="cvc" placeholder="e.g. 123" required />
+        <input type="text" name="cvc" placeholder="e.g. 123" maxlength="3" required />
       </div>
 
       <button type="submit">Confirm</button>
