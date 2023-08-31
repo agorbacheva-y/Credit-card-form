7b272f1a63c154b38966ca37b08488cf2fc949b4
agorbacheva-y <agorbacheva.y@gmail.com>
2023-08-31 14:03:27 +0200

    message text spacing

--- src/components/CardAdded.js
+++ src/components/CardAdded.js
@@ -4,8 +4,10 @@ function CardAdded() {
   return (
     <div className="message__cntr">
       <img src={complete} alt="complete"/>
-      <h1>Thank you!</h1>
-      <p>We've added your card details</p>
+      <div className="message__text">
+        <h1>Thank you!</h1>
+        <p>We've added your card details</p>
+      </div>  
       <button>Continue</button>
     </div>
   );
