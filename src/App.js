92f3f44b9c766bf4af49a3c74bd96e7f078a8c2e
agorbacheva-y <agorbacheva.y@gmail.com>
2023-09-26 22:37:28 +0200

    style attribution

--- src/App.js
+++ src/App.js
@@ -9,22 +9,30 @@ const [ showForm, setShowForm ] = useState(true);
 const [ showMsg, setShowMsg ] = useState(false);
 
   return (
-    <div className='container'>
-      <div className="card">
-        <CardFront />
-        <CardBack />
+    <>
+      <div className='container'>
+        <div className="card">
+          <CardFront />
+          <CardBack />
+        </div>
+
+        <div className="form">
+          {showForm && (
+            <CardForm setShowForm={setShowForm} setShowMsg={setShowMsg} />
+          )}
+          
+          {showMsg && (
+            <CardAdded setShowForm={setShowForm} setShowMsg={setShowMsg} />
+          )}
+        </div>
       </div>
 
-      <div className="form">
-        {showForm && (
-          <CardForm setShowForm={setShowForm} setShowMsg={setShowMsg} />
-        )}
-        
-        {showMsg && (
-          <CardAdded setShowForm={setShowForm} setShowMsg={setShowMsg} />
-        )}
+      <div class="attribution">
+        <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. </p>
+        <p>Coded by <a href="https://github.com/agorbacheva-y">AGorbacheva</a>.</p>
       </div>
-    </div>
+    </>
+    
   );
 }
 
